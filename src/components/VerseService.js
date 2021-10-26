bc95445096337c62ed3ea470f3ba7dd116bc0083
jermin2 <jermin2@gmail.com>
2021-10-27 10:30:52 +1300

    use sessionStorage instead of localStorage

--- src/components/VerseService.js
+++ src/components/VerseService.js
@@ -5,17 +5,17 @@ export default class VerseService {
     getVerse(verseRef) {
 
         // Check if we have it in the local storage
-        if(localStorage.getItem(verseRef) !== null){
+        if(sessionStorage.getItem(verseRef) !== null){
             // We need to return a Promise object to match the axios object being returned
             const myPromise = new Promise((resolve, reject) => {
-                resolve(JSON.parse(localStorage.getItem(verseRef)))
+                resolve(JSON.parse(sessionStorage.getItem(verseRef)))
               });
             return myPromise
 
         } else {
             const url = `https://api.lsm.org/recver.php?String='${verseRef}'&Out=json`;
             return axios.get(url).then(response => {
-                localStorage.setItem(verseRef, JSON.stringify(response.data)); 
+                sessionStorage.setItem(verseRef, JSON.stringify(response.data)); 
                 return response.data
 
             }).catch(e => {console.log("Failed to retrieve", verseRef)});
